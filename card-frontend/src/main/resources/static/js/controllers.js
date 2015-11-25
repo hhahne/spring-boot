12bcedc8f8f2430723f8c41225ac6e7d24c8f004
Henrik Hahne <henrik@egil.net>
2015-11-25 20:20:08 +0100

    Crude first version of HATEOAS enabled REST communication with backend server.

--- card-frontend/src/main/resources/static/js/controllers.js
+++ card-frontend/src/main/resources/static/js/controllers.js
@@ -1,6 +1,12 @@
-angular.module('card.controllers', [])
-    .controller('home', function($scope, $http) {
+angular.module('card.controllers', ["ngResource", "spring-data-rest"])
+    .controller('home', function($scope, $http, $resource, SpringDataRestAdapter) {
         $http.get('http://localhost:8082/resource').success(function(data) {
             $scope.greeting = data;
-        })
+        });
+        var response = $http.get('http://localhost:8083/decks').success(function(data) {
+            $scope.test = angular.toJson(response, true);
+        });
+        SpringDataRestAdapter.process(response).then(function(data) {
+            $scope.cards = data._embeddedItems;
+        });
     });
---
+++ card-service/src/main/java/com/henrik/card/service/RestConfiguration.java
@@ -0,0 +1,35 @@
+package com.henrik.card.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+/**
+ * This class is only needed to enable CORS (Cross Origin Resource Sharing).
+ * Spring Data Rest uses the HATEOAS style for the rest services and this is not supported by Angular out of the box.
+ * Therefore some magic is required to make that work in the controller. In addition, this is needed to make sure the data gets back to the caller. Wothout this class
+ * it would be stopped and the result would be an HTML error in loading the page and showing empty results.
+ */
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true); // you USUALLY want this
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+
+}

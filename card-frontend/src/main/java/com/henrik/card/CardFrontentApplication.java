c362593d31d2dc766b9bcb22681213fdbdd55f09
Henrik Hahne <henrik@egil.net>
2016-01-06 11:28:38 +0100

    Enabled Zuul, POM needs cleaning, properties need to be split into environments

--- card-frontend/src/main/java/com/henrik/card/CardFrontentApplication.java
+++ card-frontend/src/main/java/com/henrik/card/CardFrontentApplication.java
@@ -2,8 +2,10 @@ package com.henrik.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class CardFrontentApplication {
 
     public static void main(String[] args) {
--- card-frontend/src/main/resources/static/js/controllers.js
+++ card-frontend/src/main/resources/static/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('card.controllers', ["ngResource", "spring-data-rest"])
     .controller('home', function($scope, $http, $resource, SpringDataRestAdapter) {
-        $http.get('http://localhost:8082/resource').success(function(data) {
+        $http.get('/resource').success(function(data) {
             $scope.greeting = data;
         });
-        var response = $http.get('http://localhost:8083/decks').success(function(data) {
+        var response = $http.get('/decks').success(function(data) {
             $scope.test = angular.toJson(response, true);
         });
         SpringDataRestAdapter.process(response).then(function(data) {

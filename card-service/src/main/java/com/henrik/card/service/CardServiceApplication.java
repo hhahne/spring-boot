62fecfb1e791ffb2f5695db9a169616000a8190e
Henrik Hahne <henrik@egil.net>
2016-01-06 18:10:36 +0100

    Added default values to props, to avoid them crashing if not resolved during spring boot startup

--- card-service/src/main/java/com/henrik/card/service/CardServiceApplication.java
+++ card-service/src/main/java/com/henrik/card/service/CardServiceApplication.java
@@ -9,17 +9,17 @@ import org.springframework.core.env.Environment;
 @SpringBootApplication
 public class CardServiceApplication {
 
-    @Value("${hello}")
+    @Value("${hello:DEFAULTHELLO}")
     private String testProp;
 
-    @Value("${test}")
+    @Value("${test:DEFAULTVALUE}")
     private String cardServiceProp;
 
     @Autowired
     void setEnvironment(Environment e) {
 //        System.out.println("Property fra en property fil: " + e.getProperty("aloha"));
 //        System.out.println("Property fra spesifikk fil: " + e.getProperty("test"));
-        System.out.println("Test");
+//        System.out.println("Test");
         System.out.println(testProp);
         System.out.println(cardServiceProp);
     }

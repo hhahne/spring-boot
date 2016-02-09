99cccc1db56bc8c8ef8a1abbdc16431a32ed28c7
Henrik Hahne <henrik@egil.net>
2016-02-09 08:36:16 +0100

    Eureka discovery now working for docker containers. POM needs bad cleanup, props must be externalized

--- card-frontend/src/main/java/com/henrik/card/CardFrontentApplication.java
+++ card-frontend/src/main/java/com/henrik/card/CardFrontentApplication.java
@@ -2,10 +2,12 @@ package com.henrik.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class CardFrontentApplication {
 
     public static void main(String[] args) {

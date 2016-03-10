ea1b1070605bf34abab9cc741932d2c4cd537c32
Henrik Hahne <henrik@egil.net>
2016-03-10 07:12:08 +0100

    First try at Hystrix support for card-service

--- card-service/src/main/java/com/henrik/card/service/CardServiceApplication.java
+++ card-service/src/main/java/com/henrik/card/service/CardServiceApplication.java
@@ -3,11 +3,19 @@ package com.henrik.card.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan("com.henrik.card-service")
+@EnableAutoConfiguration
 @EnableDiscoveryClient
 public class CardServiceApplication {
 
